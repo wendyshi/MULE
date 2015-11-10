28b1412b8fdef9208149015ccdc1e547a24a5430
ql657195490 <qilei753@yahoo.com>
2015-11-09 21:47:56 -0500

    Update Store.java

--- M4Demo/src/m4demo/Store.java
+++ M4Demo/src/m4demo/Store.java
@@ -17,15 +17,18 @@ public class Store implements Serializable{
     private int smithoreq;
     private int crystiteq;
     private int muleq;
-    private final int FOOD_PRICE = 30;
-    private final int ENERGY_PRICE = 25;
-    private final int SMITHORE_PRICE = 50;
-    private final int CRYSTITE_PRICE = 100;
-    private final int MULEF_PRICE = 125;
-    private final int MULEE_PRICE = 150;
-    private final int MULES_PRICE = 175;
-    private final int MULEC_PRICE = 200;
+    private static final int FOOD_PRICE = 30;
+    private static final int ENERGY_PRICE = 25;
+    private static final int SMITHORE_PRICE = 50;
+    private static final int CRYSTITE_PRICE = 100;
+    private static final int MULEF_PRICE = 125;
+    private static final int MULEE_PRICE = 150;
+    private static final int MULES_PRICE = 175;
+   // private static final  int MULEC_PRICE = 200;
     
+    /**
+     * constructor method
+     * */
     public Store() {
         foodq = 16;
         energyq = 16;
@@ -34,6 +37,12 @@ public class Store implements Serializable{
         muleq = 25;
     }
     
+    /**
+     * method to check the player have enough funds to buy food or not
+     * @param player current player
+     * @return return true if the player have enough funds otherwise 
+     * return false
+     * */
     public boolean buyFood(Player player) {
         if (foodq > 0 && player.funds > FOOD_PRICE) {
             foodq--;
@@ -44,7 +53,13 @@ public class Store implements Serializable{
             return false;
         }
     }
-    
+
+    /**
+     * method to check the player have enough funds to buy energy or not
+     * @param player current player
+     * @return return true if the player have enough funds otherwise 
+     * return false
+     * */
     public boolean buyEnergy(Player player) {
         if (energyq > 0 && player.funds > ENERGY_PRICE) {
             energyq--;
@@ -55,7 +70,13 @@ public class Store implements Serializable{
             return false;
         }
     }
-    
+
+    /**
+     * method to check the player have enough funds to buy smithore or not
+     * @param player current player
+     * @return return true if the player have enough funds otherwise 
+     * return false
+     * */
     public boolean buySmithore(Player player) {
         if (smithoreq > 0 && player.funds > SMITHORE_PRICE) {
             smithoreq--;
@@ -66,7 +87,13 @@ public class Store implements Serializable{
             return false;
         }
     }
-    
+
+    /**
+     * method to check the player have enough funds to buy crystite or not
+     * @param player current player
+     * @return return true if the player have enough funds otherwise 
+     * return false
+     * */
     public boolean buyCrystite(Player player) {
         if (crystiteq > 0 && player.funds > CRYSTITE_PRICE) {
             crystiteq--;
@@ -77,7 +104,13 @@ public class Store implements Serializable{
             return false;
         }
     }
-    
+
+    /**
+     * method to check the player have enough funds to buy mulef or not
+     * @param player current player
+     * @return return true if the player have enough funds otherwise 
+     * return false
+     * */
     public boolean buyMuleF(Player player) {
         if (muleq > 0 && player.funds > MULEF_PRICE) {
             muleq--;
@@ -88,7 +121,13 @@ public class Store implements Serializable{
             return false;
         }
     }
-    
+
+    /**
+     * method to check the player have enough funds to buy mulee or not
+     * @param player current player
+     * @return return true if the player have enough funds otherwise 
+     * return false
+     * */
     public boolean buyMuleE(Player player) {
         if (muleq > 0 && player.funds > MULEE_PRICE) {
             muleq--;
@@ -99,7 +138,13 @@ public class Store implements Serializable{
             return false;
         }
     }
-    
+
+    /**
+     * method to check the player have enough funds to buy mules or not
+     * @param player current player
+     * @return return true if the player have enough funds otherwise 
+     * return false
+     * */
     public boolean buyMuleS(Player player) {
         if (muleq > 0 && player.funds > MULES_PRICE) {
             muleq--;
@@ -110,7 +155,13 @@ public class Store implements Serializable{
             return false;
         }
     }
-    
+
+    /**
+     * method to check the player have food to sell or not
+     * @param player current player
+     * @return return true if the player have food otherwise 
+     * return false
+     * */
     public boolean sellFood(Player player) {
         if (player.food > 0) {
             foodq++;
@@ -121,7 +172,13 @@ public class Store implements Serializable{
             return false;
         }
     }
-    
+
+    /**
+     * method to check the player have energy to sell or not
+     * @param player current player
+     * @return return true if the player have energy otherwise 
+     * return false
+     * */
     public boolean sellEnergy(Player player) {
         if (player.energy > 0) {
             energyq++;
@@ -132,7 +189,13 @@ public class Store implements Serializable{
             return false;
         }
     }
-    
+
+    /**
+     * method to check the player have smithore to sell or not
+     * @param player current player
+     * @return return true if the player have smithore otherwise 
+     * return false
+     * */
     public boolean sellSmithore(Player player) {
         if (player.smithore > 0) {
             smithoreq++;
@@ -143,7 +206,13 @@ public class Store implements Serializable{
             return false;
         }
     }
-    
+
+    /**
+     * method to check the player have crystite to sell or not
+     * @param player current player
+     * @return return true if the player have crystite otherwise 
+     * return false
+     * */
     public boolean sellCrystite(Player player) {
         if (player.crystite > 0) {
             crystiteq++;
@@ -154,7 +223,13 @@ public class Store implements Serializable{
             return false;
         }
     }
-    
+
+    /**
+     * method to check the player have mulef to sell or not
+     * @param player current player
+     * @return return true if the player have mulef otherwise 
+     * return false
+     * */
     public boolean sellMuleF(Player player) {
         if (player.mulef > 0) {
             muleq++;
@@ -165,7 +240,13 @@ public class Store implements Serializable{
             return false;
         }
     }
-    
+
+    /**
+     * method to check the player have mulee to sell or not
+     * @param player current player
+     * @return return true if the player have mulee otherwise 
+     * return false
+     * */
     public boolean sellMuleE(Player player) {
         if (player.mulee > 0) {
             muleq++;
@@ -176,7 +257,13 @@ public class Store implements Serializable{
             return false;
         }
     }
-    
+
+    /**
+     * method to check the player have mules to sell or not
+     * @param player current player
+     * @return return true if the player have mules otherwise 
+     * return false
+     * */
     public boolean sellMuleS(Player player) {
         if (player.mules > 0) {
             muleq++;
@@ -188,6 +275,9 @@ public class Store implements Serializable{
         }
     }
     
+    /**
+     * override toString() method
+     * */
     @Override
     public String toString() {
         return "Prices\nFood: 30\nEnergy: 25\nFood MULE: 125\nEnergy MULE: 125"
